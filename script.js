a4161a349d006d9cd261bd94246152c057d8fe3e
Piotr <piotrchecinski02@gmail.com>
2021-06-06 20:52:28 +0200

    Determine a winner

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const winCombos = [
 const cells = document.querySelectorAll('.cell');
 startGame();
 
+
 function startGame() {
     document.querySelector(".endgame").style.display = "none"
     origBoard = Array.from(Array(9).keys());
@@ -32,4 +33,34 @@ function turnClick(square) {
 function turn (squareId, player){
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(origBoard, player)
+    if (gameWon) gameOver(gameWon)
+}
+
+
+
+// Determine Winner
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == huPlayer ? "pink" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
 }
